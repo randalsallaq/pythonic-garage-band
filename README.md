e9d8f99065b116946d729ac40d911d9086895168
rand alsallaq <randalsallaq97@gmail.com>
2020-12-05 21:11:13 +0200

    solved

---
+++ README.md
@@ -0,0 +1,18 @@
+# Pythonic Garage Band
+
+
+[Repository Link](https://github.com/randalsallaq/pythonic-garage-band)
+
+Live [URL](https://randalsallaq.github.io/garage/)
+
+## Lab 04:
+## Classes, and Fixtures
+
+Creating a Garage Band with Object Oriented Programming. 
+
+- ***Musician***: creates instances of Musicians.
+- ***Guitarist***: inherits properties of Musician.
+- ***Bassist***: inherits properties of Musician.
+- ***Drummer***: inherits properties of Musician.
+- ***Band***: creates instances of the Band.
+
---
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -0,0 +1,106 @@
+from abc import abstractmethod, ABC
+
+class Musician(ABC):
+
+    def __init__(self,name):
+        self.name = name
+
+    def play_solo(self):
+        pass
+        
+    def get_instrument(self):
+        pass
+
+    def __str__(self):
+            
+        return 'My name is '+self.name+' and I play '+self.get_instrument()
+
+        
+    def __repr__(self):
+        
+        return self.__class__.__name__+' instance. Name = '+self.name
+
+
+
+class Guitarist(Musician):
+    '''
+    to create new guitarists
+    '''
+    def play_solo(self):
+        return "face melting guitar solo"
+
+    def get_instrument(self):
+        return "guitar" 
+
+
+class Bassist(Musician):
+    '''
+    to create new bassists
+    '''
+    def play_solo(self):
+        return "bom bom buh bom"
+    def get_instrument(self):
+        return "bass" 
+
+
+class Drummer(Musician):
+    '''
+    to create new drummers
+    '''
+    def play_solo(self):
+        return "rattle boom crash"
+    def get_instrument(self):
+        return "drums" 
+
+
+
+class Band(ABC):
+    all_members=[]
+
+    def __init__(self, name, members):
+        self.name = name 
+        self.members = members
+        Band.all_members.append(self)
+        
+
+
+    def play_solos(self):
+        solos = []
+
+        if len(self.members) !=0:
+            for i in self.members:
+                solos.append(i.play_solo())
+            return solos
+
+
+        else:
+            return 'No members yet'
+
+
+    def __str__(self):
+        return (' Band '+ self.name + ' Members: '+self.members).strip()
+
+    def __repr__(self):
+        return (' Band '+ self.name + ' Members: '+self.members).strip()
+        
+
+    @classmethod
+    def to_list(cls):
+        return cls.all_members
+
+
+
+
+if __name__ == "__main__":
+    x = Guitarist("jason")
+    b = Bassist("adi")
+    c = Drummer("katy")
+
+    miami_Band = Band('miami',[])
+    print(miami_Band.to_list())
+
+
+        
+        
+
+
